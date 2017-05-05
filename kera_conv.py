4fa02953b279e44ca79983fde3a7d4cf6527057b
linux-devil <hrsht.sarma@gmail.com>
2017-05-05 12:33:04 +0530

    Update kera_conv.py
    added pool and dropout

--- kera_conv.py
+++ kera_conv.py
@@ -22,6 +22,8 @@ input_shape = (32, 32, 3)
 #Convolutional Neural Network in Keras Here
 model = Sequential()
 model.add(Conv2D(32,3, 3,activation='relu',input_shape=input_shape,border_mode='valid'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 model.add(Flatten())
 model.add(Dense(128))
 model.add(Activation('relu'))
